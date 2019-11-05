22ed9093aef11166d3e851a5d796596088a590a1
Tim Seed <TSEED@bloomberg.net>
2019-11-05 14:45:32 +0800

    Cleanup

--- tim/web/controllers/app.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, Response
-from flask_restplus import Api
-
-app = Flask(__name__)
-api = Api(app)
-
--- tim/web/controllers/obj_a.py
+++
@@ -1,7 +0,0 @@
-from flask_restplus import Resource, Api
-from tim.web.controllers import api
-
-@api.route('/hello','/hiroy')
-class Hello(Resource):
-    def get(self):
-        return { 'hello': 'World' }
