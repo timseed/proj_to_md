b44d8ed8853d1ee913ad811d87f08c3c010920cf
Tim Seed <tim@sy-edm.com>
2020-04-19 16:21:52 +0800

    Setup adds script now

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='proj_to_md',
     version='',
     packages=['tim',  'tim.utils', 'tim.utils.proj2md'],
-    url='',
+    entry_points={'console_scripts': ['proj2md=tim.utils.proj2md.__main__:main']},
     license='',
     author='tim seed',
-    author_email='',
-    description=''
+    author_email='tim@sy-edm.com',
+    description='Quickly dump a python project into a Markdown output format.'
 )
--- tim/utils/proj2md/__main__.py
+++ tim/utils/proj2md/__main__.py
@@ -1,16 +1,21 @@
 from tim.utils.proj2md import dir_tree, get_arguments
 from tim.utils.proj2md.fakedown import *
 
-args = get_arguments()
-t, files = dir_tree(args.dir, args.types)
-print(level(1, "Project " + args.dir))
-print(text("\n Layout of this project is ...\n"))
-print(level(3, "Layout"))
-print(code('text', t))
-print(level(2, "Code Listing"))
-for f in files:
-    print(level(3, f.split('/')[-1]))
-    print(dump_file('python', f))
-from pprint import pprint
-pprint(args)
-print("Hi")
+def main():
+    args = get_arguments()
+    t, files = dir_tree(args.dir, args.types)
+    print(level(1, "Project " + args.dir))
+    print(text("\n Layout of this project is ...\n"))
+    print(level(3, "Layout"))
+    print(code('text', t))
+    print(level(2, "Code Listing"))
+    for f in files:
+        print(level(3, f.split('/')[-1]))
+        print(dump_file('python', f))
+    from pprint import pprint
+    pprint(args)
+    print("Hi")
+
+
+if __name__ == "__main__":
+    main()
